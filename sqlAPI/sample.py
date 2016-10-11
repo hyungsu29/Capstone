d287764a4747f1c8857b5f888156305ddbc7b0c1
SeahunOh <shpik.korea@gmail.com>
2016-10-11 16:16:42 +0900

    sqlAPI

--- sqlAPI/sample.py
+++ sqlAPI/sample.py
@@ -4,4 +4,8 @@ connect_db()
 title = query('select * from DB1')[0]['title']
 print title
 close_db()
+'''
+shpik@shpik-400-470kr:~/Capstone/sqlAPI$ python sample.py 
+TEKKEN STRIKE ‘qudans’ 손병문, ‘물골드’ 한재균의 활약이 돋보인 8강 B조 경기
+'''
 
